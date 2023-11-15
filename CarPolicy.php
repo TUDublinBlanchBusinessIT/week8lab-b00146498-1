8eb906e07b3b45d9618b9c0f7f034a626bf3534e
b00146498@mytudublin.ie <b00146498@mytudublin.ie>
2023-11-15 10:02:40 +0000

    got part 1 car policy to display policy number and years of claims

---
+++ CarPolicy.php
@@ -0,0 +1,36 @@
+<?php
+class CarPolicy
+{
+    private $policyNumber = 0;
+    private $yearlyPremium = 0;
+    private $dateOfLastClaim = 0;
+    
+    public function __construct($policyNumber, $yearlyPremium)
+    {
+        $this->policyNumber = $policyNumber;
+        $this->yearlyPremium = $yearlyPremium;
+    }
+    
+    public function setDateOfLastClaim($dateOfLastClaim)
+    {
+        $this->dateOfLastClaim = $dateOfLastClaim;
+    }
+        
+    public function getTotalYearsNoClaims()
+    {
+     $currentDate = new DateTime();
+     $lastDate= new DateTime($this->dateOfLastClaim);
+     $interval = $currentDate->diff($lastDate);
+     return $interval->format("%y");
+    }
+    
+    public function __tostring()
+    {
+        return $this->policyNumber;
+    }
+}
+
+
+
+
+?>
